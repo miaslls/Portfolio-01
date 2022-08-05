7d1232ffc0796fba8517d3402a63f9b987530df1
miaslls <97785569+miaslls@users.noreply.github.com>
2022-08-05 07:13:50 -0300

    mf bug 🐞

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,1 +1,27 @@
 'use strict';
+
+let projectIndex = 1;
+
+showProjects(projectIndex);
+
+function changeProject(n) {
+  showProjects((projectIndex += n));
+}
+
+function showProjects(n) {
+  let projects = document.getElementsByClassName('project-container');
+
+  if (n > projects.length) {
+    projectIndex = 1;
+  }
+
+  if (n < 1) {
+    projectIndex = projects.length;
+  }
+
+  for (let i = 0; i < projects.length; i++) {
+    projects[i].style.display = 'none';
+  }
+
+  projects[projectIndex - 1].style.display = 'flex';
+}
